e622c4e7f1a40bd88ecc2977c9e04f34373af228
“WalterAbregu” <abreguwalter0709@gmail.com>
2020-05-21 22:58:28 -0500

    curso capitulo 46

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES(1,'Walter','Abregu','abreguwalter0709@gmail.com','2017-09-28');
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES(2,'Ashly','Aguilar','aguilarash@gmail.com','2010-01-11');
