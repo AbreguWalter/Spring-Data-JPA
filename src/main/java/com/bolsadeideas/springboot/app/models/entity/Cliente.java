70794c42ef59240761ddf43130cd8c1e6fe13dda
“WalterAbregu” <abreguwalter0709@gmail.com>
2020-05-24 15:19:36 -0500

    episodio 59 curso de spring

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -8,31 +8,50 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 
 @Entity
 @Table(name="clientes")
 public class Cliente implements Serializable {
 	
+	private static final long serialVersionUID = 4327690299451102763L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 //	@Column(name="nombre_cliente") para cambiar el campo en la tabla de la BD
+	@NotEmpty
 	private String nombre;
 	
+	@NotEmpty // valida que no sea nulo el campo y que venga mas de 1 caracter
 	private String apellido;
 	
+	@NotEmpty
+	@Email
 	private String email;
 	
+	@NotNull
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE) //forma para la fecha en la BD
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 	
-	private static final long serialVersionUID = 4327690299451102763L;
-
+	//Se ejecutara el metodo antes del registro de la BD
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+	
 	public Long getId() {
 		return id;
 	}
