5d0453a74e743b03299d27f94db15b7be2d04eed
“WalterAbregu” <abreguwalter0709@gmail.com>
2020-06-07 02:01:25 -0500

    buscador de productos implementado

--- src/main/java/com/bolsadeideas/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/FacturaController.java
@@ -1,17 +1,22 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.entity.Factura;
+import com.bolsadeideas.springboot.app.models.entity.Producto;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
 
 @Controller
@@ -22,8 +27,10 @@ public class FacturaController {
 	@Autowired
 	private IClienteService clienteService;
 	
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
 	@GetMapping("/form/{clienteId}")
-	public String crear(@PathVariable(value="clienteid") Long clienteId, Map<String,Object> model , RedirectAttributes flash) {
+	public String crear(@PathVariable(value="clienteId") Long clienteId, Map<String,Object> model , RedirectAttributes flash) {
 		Cliente cliente = clienteService.findOne(clienteId);
 		
 		if(cliente == null) {
@@ -39,4 +46,14 @@ public class FacturaController {
 		
 		return "factura/form";
 	}
+	
+	//Cuando usamos ajax , es importante usar ResponseBody para que no pida devolver a una vista 
+	@GetMapping(value = "/cargar-productos/{term}", produces= {"application/json"})
+	public @ResponseBody List<Producto> cargarProductos(@PathVariable String term){
+		log.info("term ::::::::::::::::::: " + term);
+		return clienteService.findByNombre(term);
+	}
+
+	
+	
 }
